a48ef4e54269a86d745ef049ffc218d9ebf49403
ahmed <foudiahmed82@gmail.com>
2024-11-12 17:24:00 +0100

    second commit

---
+++ src/main/java/ahmed/foudi/MRW/MrwApplication.java
@@ -0,0 +1,13 @@
+package ahmed.foudi.MRW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrwApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ahmed/foudi/MRW/entities/WaitingRoom.java
@@ -0,0 +1,11 @@
+package ahmed.foudi.MRW.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class WaitingRoom {
+    @Id
+    @GeneratedValue()
+}
