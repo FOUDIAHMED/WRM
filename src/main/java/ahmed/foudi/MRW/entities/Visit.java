6f1fd21595652426325e225180e49eb2ceb2d7e7
ahmed <foudiahmed82@gmail.com>
2024-11-14 10:03:05 +0100

    visite  entity

---
+++ src/main/java/ahmed/foudi/MRW/entities/Visit.java
@@ -0,0 +1,32 @@
+package ahmed.foudi.MRW.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Entity
+
+@Data
+public class Visit {
+    @EmbeddedId
+    private EmbdedId id;
+
+    @ManyToOne
+    @MapsId("visitorId")
+    @JoinColumn(name = "visitor_id")
+    private Visitor visitor;
+
+    @ManyToOne
+    @MapsId("waitingRoomId")
+    @JoinColumn(name = "waitingRoomId")
+    private WaitingRoom waitingRoom;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Status status;
+    private byte priority;
+    private Duration estimatedProcessTime;
+
+}
