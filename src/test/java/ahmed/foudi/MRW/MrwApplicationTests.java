3ee387a299862d2be759ed6be2c3e578296bedbe
ahmed <foudiahmed82@gmail.com>
2024-11-15 16:13:22 +0100

    docker configuration

--- src/test/java/ahmed/foudi/MRW/MrwApplicationTests.java
+++ src/test/java/ahmed/foudi/MRW/MrwApplicationTests.java
@@ -1,13 +1,13 @@
-package ahmed.foudi.MRW;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MrwApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ahmed.foudi.MRW;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MrwApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
