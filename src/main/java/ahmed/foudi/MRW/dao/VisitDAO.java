06d0eee01192e3501181288713736bf13b47ed00
ahmed <foudiahmed82@gmail.com>
2024-11-15 09:36:25 +0100

    daos

---
+++ src/main/java/ahmed/foudi/MRW/dao/VisitDAO.java
@@ -0,0 +1,8 @@
+package ahmed.foudi.MRW.dao;
+
+import ahmed.foudi.MRW.entities.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitDAO extends JpaRepository<Visit, Long> {
+
+}
---
+++ src/main/java/ahmed/foudi/MRW/dao/WaitingRoomDAO.java
@@ -0,0 +1,7 @@
+package ahmed.foudi.MRW.dao;
+
+import ahmed.foudi.MRW.entities.WaitingRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WaitingRoomDAO extends JpaRepository<WaitingRoom, Long> {
+}
